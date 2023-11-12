78c8cd20c64881ec69d55e2da5dca04ca22647cb
Omar G <Sama34@users.noreply.github.com>
2023-11-12 01:43:47 -0800

    Drop compatibility to PHP7.0
    Update standard to PSR-12

--- Upload/inc/plugins/ougcHideAdminLoc/core.php
+++ Upload/inc/plugins/ougcHideAdminLoc/core.php
@@ -2,62 +2,64 @@
 
 /***************************************************************************
  *
- *	OUGC Hide Administrator Location (/inc/plugins/ougcHideAdminLoc/core.php)
- *	Author: Omar Gonzalez
- *	Copyright: © 2016 - 2023 Omar Gonzalez
+ *    OUGC Hide Administrator Location (/inc/plugins/ougcHideAdminLoc/core.php)
+ *    Author: Omar Gonzalez
+ *    Copyright: © 2016 - 2023 Omar Gonzalez
  *
- *	Website: https://ougc.network
+ *    Website: https://ougc.network
  *
- *	Hide administrator's location from the Who Is Online (WOL) list.
+ *    Hide administrator's location from the Who Is Online (WOL) list.
  *
  ***************************************************************************
-
  ****************************************************************************
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  ****************************************************************************/
 
 namespace OUGCHideAdminLoc\Core;
 
-function loadLanguage() : true {
+function loadLanguage(): bool
+{
     global $lang;
 
-	if(!isset($lang->setting_group_ougc_hal)) {
-		$lang->load('ougc_hal');
-	}
+    if (!isset($lang->setting_group_ougc_hal)) {
+        $lang->load('ougc_hal');
+    }
 
     return true;
 }
 
-function pluginLibraryRequirements() : object {
-    return (object) \OUGCHideAdminLoc\Admin\pluginInfo()['pl'];
+function pluginLibraryRequirements(): object
+{
+    return (object)\OUGCHideAdminLoc\Admin\pluginInfo()['pl'];
 }
 
-function loadPluginLibrary(bool $doCheck = true) : bool {
-	global $PL, $lang;
+function loadPluginLibrary(bool $doCheck = true): bool
+{
+    global $PL, $lang;
 
-	loadLanguage();
+    loadLanguage();
 
-	if($fileExists = file_exists(PLUGINLIBRARY)) {
+    if ($fileExists = file_exists(PLUGINLIBRARY)) {
         ($PL instanceof PluginLibrary) or require_once PLUGINLIBRARY;
-	}
+    }
 
-	if(!$doCheck) {
-		return false;
-	}
+    if (!$doCheck) {
+        return false;
+    }
 
-	if(!$fileExists || $PL->version < pluginLibraryRequirements()->version) {
-		flash_message(
+    if (!$fileExists || $PL->version < pluginLibraryRequirements()->version) {
+        flash_message(
             $lang->sprintf(
                 $lang->ougc_hal_pluginlibrary,
                 pluginLibraryRequirements()->url,
@@ -66,30 +68,30 @@ function loadPluginLibrary(bool $doCheck = true) : bool {
             'error'
         );
 
-		admin_redirect('index.php?module=config-plugins');
-	}
+        admin_redirect('index.php?module=config-plugins');
+    }
 
     return true;
 }
 
-function addHooks(string $namespace) : true {
+function addHooks(string $namespace): true
+{
     global $plugins;
 
     $namespaceLowercase = strtolower($namespace);
     $definedUserFunctions = get_defined_functions()['user'];
 
-	foreach($definedUserFunctions as $callable) {
+    foreach ($definedUserFunctions as $callable) {
         $namespaceWithPrefixLength = strlen($namespaceLowercase) + 1;
 
-		if(substr($callable, 0, $namespaceWithPrefixLength) == $namespaceLowercase.'\\') {
+        if (substr($callable, 0, $namespaceWithPrefixLength) == $namespaceLowercase . '\\') {
             $hookName = substr_replace($callable, '', 0, $namespaceWithPrefixLength);
 
             $priority = substr($callable, -2);
 
-			if(is_numeric(substr($hookName, -2))) {
+            if (is_numeric(substr($hookName, -2))) {
                 $hookName = substr($hookName, 0, -2);
-			}
-			else {
+            } else {
                 $priority = 10;
             }
 
@@ -100,10 +102,11 @@ function addHooks(string $namespace) : true {
     return true;
 }
 
-function getSetting( string $settingKey = '') : string {
-	global $mybb;
+function getSetting(string $settingKey = ''): string
+{
+    global $mybb;
 
-	$string = 'OUGC_HAL_'.strtoupper($settingKey);
+    $string = 'OUGC_HAL_' . strtoupper($settingKey);
 
-	return defined($string) ? constant($string) : (string) $mybb->settings['ougc_hal_'.$settingKey];
+    return defined($string) ? constant($string) : (string)$mybb->settings['ougc_hal_' . $settingKey];
 }
--- Upload/inc/plugins/ougc_hal.php
+++ Upload/inc/plugins/ougc_hal.php
@@ -2,29 +2,28 @@
 
 /***************************************************************************
  *
- *	OUGC Hide Administrator Location (/inc/plugins/ougc_hal.php)
- *	Author: Omar Gonzalez
- *	Copyright: © 2016 - 2023 Omar Gonzalez
+ *    OUGC Hide Administrator Location (/inc/plugins/ougc_hal.php)
+ *    Author: Omar Gonzalez
+ *    Copyright: © 2016 - 2023 Omar Gonzalez
  *
- *	Website: https://ougc.network
+ *    Website: https://ougc.network
  *
- *	Hide administrator's location from the Who Is Online (WOL) list.
+ *    Hide administrator's location from the Who Is Online (WOL) list.
  *
  ***************************************************************************
-
  ****************************************************************************
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  ****************************************************************************/
 
 // Die if IN_MYBB is not defined, for security reasons.
@@ -36,37 +35,40 @@ const OUGC_HAL_SETTING_GIDS = []; // comma separated groups (gid) to hide
 
 const OUGC_HAL = MYBB_ROOT . 'inc/plugins/ougcHideAdminLoc';
 
-require_once OUGC_HAL.'/core.php';
+require_once OUGC_HAL . '/core.php';
 
 // PLUGINLIBRARY
-defined('PLUGINLIBRARY') or define('PLUGINLIBRARY', MYBB_ROOT.'inc/plugins/pluginlibrary.php');
+defined('PLUGINLIBRARY') or define('PLUGINLIBRARY', MYBB_ROOT . 'inc/plugins/pluginlibrary.php');
 
 // Add our hooks
-if(defined('IN_ADMINCP')) {
-    require_once OUGC_HAL.'/admin.php';
-}
-else {
-    require_once OUGC_HAL.'/forumHooks.php';
+if (defined('IN_ADMINCP')) {
+    require_once OUGC_HAL . '/admin.php';
+} else {
+    require_once OUGC_HAL . '/forumHooks.php';
 
     \OUGCHideAdminLoc\Core\addHooks('OUGCHideAdminLoc\ForumHooks');
 }
 
 // Plugin API
-function ougc_hal_info() : array {
+function ougc_hal_info(): array
+{
     return \OUGCHideAdminLoc\Admin\pluginInfo();
 }
 
 // _activate() routine
-function ougc_hal_activate() : true {
+function ougc_hal_activate(): bool
+{
     return \OUGCHideAdminLoc\Admin\pluginActivate();
 }
 
 // _is_installed() routine
-function ougc_hal_is_installed() : bool {
+function ougc_hal_is_installed(): bool
+{
     return \OUGCHideAdminLoc\Admin\pluginIsInstalled();
 }
 
 // _uninstall() routine
-function ougc_hal_uninstall() : true {
+function ougc_hal_uninstall(): bool
+{
     return \OUGCHideAdminLoc\Admin\pluginUninstall();
 }
